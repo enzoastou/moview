30ab6683efc811e08ed3c53544cbeb11a2f384f7
Enzo Fragale <fragale.enzo@gmail.com>
2022-11-15 11:39:50 +0100

    Implemented API calls to retrieve titles, ids and ratings from user search

--- main.py
+++ main.py
@@ -4,3 +4,24 @@ Authors: Driss BENSAID & Enzo Fragale
 Date of creation: 15/11/2022
 
 """
+import requests
+import json
+
+
+text="tenet"
+responseMovieSearch = requests.get("https://imdb-api.com/API/SearchMovie/k_djox3zay/" 
+                        + text)
+movies = responseMovieSearch.json()["results"]
+titles = [movie["title"] for movie in movies]
+ids = [movie["id"] for movie in movies]
+
+print(titles[0])
+print(ids[0])
+
+responseRatings = requests.get("https://imdb-api.com/API/Ratings/k_djox3zay/"
+                               + ids[0])
+ratings = responseRatings.json()
+
+print(ratings[5:-1])
+
+
