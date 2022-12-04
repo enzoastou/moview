b4a3037005bff1da3132113a1e2c8626d462b76b
Enzo Fragale <fragale.enzo@gmail.com>
2022-12-04 18:56:25 +0100

    Added docstring documentation within modules

--- api_calls/movie_search.py
+++ api_calls/movie_search.py
@@ -10,6 +10,10 @@ import os
 from dotenv import load_dotenv
 
 class MovieSearch:
+    """
+    Contains various functions to retrieve results from a movie search request
+    on the imdb API.
+    """
     _searchTitle=""
     _responseMovieSearch=""
     
@@ -17,33 +21,104 @@ class MovieSearch:
         return super().__new__(cls)
     
     def __init__(self, title):
+        """
+        Initalises the class instance based on the title of the movie to be
+        searched.
+
+        Parameters
+        ----------
+        title : String
+            Represents the title of the movie to be searched.
+
+        Returns
+        -------
+        None.
+
+        """
         self._searchTitle = title
         load_dotenv()
         apiKey=os.getenv("KEY")
-        self._responseMovieSearch = requests.get("https://imdb-api.com/API/SearchMovie/"
-                                                 + apiKey + "/" 
-                                                 + self._searchTitle)
+        self._responseMovieSearch = requests.get(
+            "https://imdb-api.com/API/SearchMovie/" + apiKey + "/"
+            + self._searchTitle)
         
     def __repr__(self) -> str:
-        return f"{type(self).__name__}(type={self._type}, message={self._message}, \
-                created_at={self._created_at})"
+        return f"{type(self).__name__}(type={self._type}, \
+                message={self._message}, created_at={self._created_at})"
     
     def get_response(self):
-        return self._responseMovieSearch.json()
+        """
+        Gets and returns the response of the movie search request of the title
+        passed when initialising the class instance on the imdb API as a JSON
+        object.
+        
+        Returns
+        -------
+        response : String
+            The response of the movie search request of the title passed when 
+            initialising the class instance on the imdb API as a JSON object.
+
+        """
+        response = self._responseMovieSearch.json()
+        return response
     
     def get_movies(self):
+        """
+        Gets and returns all the movies matching the title passed when
+        initialising the class instance as a JSON array.
+        
+        Returns
+        -------
+        movies : String
+            All the movies matching the title passed when initialising the
+            class instance as a JSON array.
+
+        """
         movies =  self.get_response()["results"]
         return movies
     
     def get_titles(self):
+        """
+        Gets and returns all the movie titles matching the title passed when
+        intialising the class instance as a list of strings.
+
+        Returns
+        -------
+        titles : String list
+            All the movie titles matching the title passed when initialising
+            the class instance as a list of strings.
+
+        """
         movies = self.get_movies()
         titles = [movie["title"] for movie in movies]
         return titles
     
     def get_ids(self):
+        """
+        Gets and returns all the movie ids matching the title passed when
+        intialising the class instance as a list of strings.
+
+        Returns
+        -------
+        titles : String list
+            All the movie ids matching the title passed when initialising
+            the class instance as a list of strings.
+
+        """
         movies = self.get_movies()
         ids = [movie["id"] for movie in movies]
         return ids
     
     def get_best_match_id(self):
+        """
+        Gets and returns the id of the movie with the best match with the title
+        passed when intialising the class instance as a string.
+
+        Returns
+        -------
+        titles : String
+            The id of the movie with the best match with the title passed when 
+            intialising the class instance as a string.
+
+        """
         return self.get_ids()[0]
--- api_calls/ratings_retrieval.py
+++ api_calls/ratings_retrieval.py
@@ -10,6 +10,10 @@ import os
 from dotenv import load_dotenv
 
 class RatingsRetrieval:
+    """
+    Contains various functions to retrieve ratings from a movie id on the imdb
+    API.
+    """
     _id=""
     _responseRatings=""
     
@@ -17,21 +21,59 @@ class RatingsRetrieval:
         return super().__new__(cls)
     
     def __init__(self, id):
+        """
+        Initalises the class instance based on the id of the movie whose
+        ratings need to be retrieved.
+
+        Parameters
+        ----------
+        id : String
+            Represents the id of the movie whose ratings need to be retrieved.
+
+        Returns
+        -------
+        None.
+
+        """
         self._id = id
         load_dotenv()
         apiKey=os.getenv("KEY")
-        self._responseRatings = requests.get("https://imdb-api.com/API/Ratings/"
-                                                 + apiKey + "/" 
-                                                 + self._id)
+        self._responseRatings = requests.get(
+            "https://imdb-api.com/API/Ratings/" + apiKey + "/" 
+            + self._id)
         
     def __repr__(self) -> str:
-        return f"{type(self).__name__}(type={self._type}, message={self._message}, \
-                created_at={self._created_at})"
+        return f"{type(self).__name__}(type={self._type}, \
+            message={self._message}, created_at={self._created_at})"
     
     def get_response(self):
-        return self._responseRatings.json()
+        """
+        Gets and returns the response of the ratings retrieval request of the
+        id passed when initialising the class instance on the imdb API as a
+        JSON object.
+        
+        Returns
+        -------
+        response : String
+            The response of the ratings retrieval request of the id passed when
+            initialising the class instance on the imdb API as a JSON object.
+            
+        """
+        response = self._responseRatings.json()
+        return response
     
     def get_ratings_list(self):
+        """
+        Gets and returns all the ratings on the id passed when intialising the 
+        class instance as a list of strings.
+
+        Returns
+        -------
+        ratingsList : String list
+            All the ratings on the id passed when intialising the class
+            instance as a list of strings.
+
+        """
         ratings =  self.get_response()
         ratingsList = list(ratings.items())[5:-1]
         return ratingsList
